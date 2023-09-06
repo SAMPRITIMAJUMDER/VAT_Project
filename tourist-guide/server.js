c630efd2997c39385a071d9139f8335389dc153c
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2023-09-07 00:09:23 +0530

    server opens home page

--- tourist-guide/server.js
+++ tourist-guide/server.js
@@ -3,6 +3,7 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
+const homeRouter = require("./routes/home");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -10,6 +11,6 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("pubilc"));
 
-app.use("/", indexRouter);
+app.use("/", homeRouter);
 
 app.listen(process.env.PORT || 3000);
